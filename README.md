913878588a5a3fb1e658a7c0a0015ba8aa6dcfef
Rik van Toor <rik.van.toor@gmail.com>
2018-04-09 02:28:19 +0200

    Updated README and data

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Submit
 
 This is a submission system built in Haskell, using Servant, Persistent, and PostreSQL.
+
+# Install/build guide
+
+1. Clone this repository if you haven't already and navigate to the folder in which you've cloned it in your terminal.
+1. The current release of _Esqueleto_ does not support the newest version of _Persistent_ yet. We have to download and build _Esqueleto_ manually.
+ 
+    Run `git clone https://github.com/bitemyapp/esqueleto.git esqueleto`
+1. Install PostgreSQL on your system. For arch-based systems this is as simple as
+
+    `sudo pacman -S postgresql`
+1. Run `sudo -i -u postgres` to change to the postgres user that was created during the install of PostgreSQL.
+1. As the postgres user, run `initdb --locale en_US.UTF-8 -E UTF8 -D '/var/lib/postgres/data'` to initialize a database.
+1. Enter `exit` to return to your normal user account.
+1. Run `sudo systemctl start postgres.service` to start Postgres.
+1. Go back to the postgres user: `sudo -i -u postgres`
+1. Run `createuser --interactive`. Call the user 'submit' and make it a superuser. Do not give it a password.
+1. Run `createdb submit -U submit` to create a database for the application
+1. Go back to your own user by entering `exit`.
+1. `stack setup`
+1. `./run` builds and runs the application. This builds up the database tables as well.
+1. Now, to fill the database with some sample data, press `Ctrl+C` to stop the server once it's running and enter `psql -f database/data.sql -d submit -U submit`.
+1. Restart the application by entering `./run` again.
+1. Go to `http://localhost:8081` in your browser and check out the website. The sample accounts are:
+
+    username | password | role
+    --- | --- | ---
+    wouter | test | Teacher
+    alejandro | test | Teacher
+    4239776 | test | Student
--- database/data.sql
+++ database/data.sql
@@ -1,13 +1,206 @@
-INSERT INTO users VALUES ('notwouter','Not Wouter Swierstra', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'), ('notalejandro', 'Not Alejandro Serrano Mena', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'), ('notrik', 'Not Rik van Toor', '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
+--
+-- PostgreSQL database dump
+--
 
-INSERT INTO students VALUES ('notrik', 2014);
+-- Dumped from database version 10.3
+-- Dumped by pg_dump version 10.3
 
-INSERT INTO teachers VALUES ('notwouter', 'BBG-570'), ('notalejandro', 'BBG-570');
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
 
-INSERT INTO courses VALUES ('INFOAFP', 'Some stuff about Haskell and Agda. Pretty cool.', 'Advanced Functional Programming');
+--
+-- Data for Name: course; Type: TABLE DATA; Schema: public; Owner: submit
+--
 
-INSERT INTO teachers_courses VALUES ('notwouter','INFOAFP'), ('notalejandro','INFOAFP');
+INSERT INTO public.course (id, coursecode, description, coursename) VALUES (1, 'INFOAFP', 'Haskell and Agda, but _advanced_', 'Advanced Functional Programming');
+INSERT INTO public.course (id, coursecode, description, coursename) VALUES (2, 'INFOMBD', 'In this course you will probably approximately learn about probably approximately correct learning.', 'Big Data');
+INSERT INTO public.course (id, coursecode, description, coursename) VALUES (3, 'INFOFP', 'Haskell, but not quite as _advanced_.', 'Functioneel Programmeren');
 
-INSERT INTO following_courses VALUES ('notrik','INFOAFP');
 
-INSERT INTO assignments (coursecode,name,description,deadline,nrofstudents) VALUES ('INFOAFP', 'Cool cat pictures', 'Send me some real cool cat pictures. Like at least three or something.', '2018-04-20'::timestamp, 3);
+--
+-- Data for Name: assignment; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.assignment (id, coursecode, name, description, deadline, nrofstudents) VALUES (1, 1, 'Solve P=NP', 'Just do it man. Come on', '2018-04-10 00:00:00+02', 1);
+INSERT INTO public.assignment (id, coursecode, name, description, deadline, nrofstudents) VALUES (2, 1, 'Cool cat pictures', 'Send me some cool cat pictures.', '2018-04-10 00:00:00+02', 1);
+INSERT INTO public.assignment (id, coursecode, name, description, deadline, nrofstudents) VALUES (3, 3, 'Hello World', 'Write Hello world in Haskell', '2018-04-10 00:00:00+02', 1);
+
+
+--
+-- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public."user" (id, name, username, password) VALUES (1, 'Rik', '4239776', 'test');
+INSERT INTO public."user" (id, name, username, password) VALUES (2, 'Wouter', 'wouter', 'test');
+INSERT INTO public."user" (id, name, username, password) VALUES (3, 'Alejandro', 'alejandro', 'test');
+INSERT INTO public."user" (id, name, username, password) VALUES (4, 'Arno', 'arno', 'test');
+INSERT INTO public."user" (id, name, username, password) VALUES (5, 'Ad', 'ad', 'test');
+
+
+--
+-- Data for Name: student; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.student (id, userid, enrollmentyear) VALUES (1, 1, 2014);
+
+
+--
+-- Data for Name: assists; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+
+
+--
+-- Data for Name: submission; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.submission (id, lastchangedtime, readme, grade, gradedby, assignmentid) VALUES (1, '2018-04-05 05:35:00+02', 'For this project I did a bit of this and a bit of that.', 7.5, 2, 1);
+
+
+--
+-- Data for Name: file; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.file (id, isbinary, filesize, filename, submissionid, filepath) VALUES (1, false, 31, 'Solution.md', 1, 'assets/files/servant-multipart26490-4.buf');
+
+
+--
+-- Data for Name: follows; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.follows (id, studentid, courseid) VALUES (2, 1, 1);
+INSERT INTO public.follows (id, studentid, courseid) VALUES (1, 1, 2);
+
+
+--
+-- Data for Name: submits; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.submits (id, studentid, submissionid) VALUES (1, 1, 1);
+
+
+--
+-- Data for Name: teacher; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.teacher (id, userid, office) VALUES (1, 2, 'BBG-570');
+INSERT INTO public.teacher (id, userid, office) VALUES (2, 3, 'BBG-570');
+INSERT INTO public.teacher (id, userid, office) VALUES (3, 4, 'BBG-562');
+INSERT INTO public.teacher (id, userid, office) VALUES (4, 5, 'BBG-563');
+
+
+--
+-- Data for Name: teaches; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.teaches (id, teacherid, courseid) VALUES (3, 1, 1);
+INSERT INTO public.teaches (id, teacherid, courseid) VALUES (4, 2, 1);
+INSERT INTO public.teaches (id, teacherid, courseid) VALUES (1, 3, 2);
+INSERT INTO public.teaches (id, teacherid, courseid) VALUES (2, 4, 2);
+INSERT INTO public.teaches (id, teacherid, courseid) VALUES (5, 2, 3);
+
+
+--
+-- Data for Name: user_password; Type: TABLE DATA; Schema: public; Owner: submit
+--
+
+INSERT INTO public.user_password (id, userid, password) VALUES (1, 1, 'test');
+INSERT INTO public.user_password (id, userid, password) VALUES (2, 2, 'test');
+INSERT INTO public.user_password (id, userid, password) VALUES (3, 3, 'test');
+
+
+--
+-- Name: assignment_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.assignment_id_seq', 1, false);
+
+
+--
+-- Name: assists_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.assists_id_seq', 1, false);
+
+
+--
+-- Name: course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.course_id_seq', 2, true);
+
+
+--
+-- Name: file_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.file_id_seq', 8, true);
+
+
+--
+-- Name: follows_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.follows_id_seq', 2, true);
+
+
+--
+-- Name: student_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.student_id_seq', 1, true);
+
+
+--
+-- Name: submission_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.submission_id_seq', 1, false);
+
+
+--
+-- Name: submits_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.submits_id_seq', 1, false);
+
+
+--
+-- Name: teacher_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.teacher_id_seq', 2, true);
+
+
+--
+-- Name: teaches_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.teaches_id_seq', 4, true);
+
+
+--
+-- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.user_id_seq', 3, true);
+
+
+--
+-- Name: user_password_id_seq; Type: SEQUENCE SET; Schema: public; Owner: submit
+--
+
+SELECT pg_catalog.setval('public.user_password_id_seq', 3, true);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
